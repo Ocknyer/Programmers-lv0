df7e52de9d3357658ba1f9663a32d15514e4e314
Ocknyer <kdu4045@naver.com>
2022-10-21 01:15:55 +0900

    fix: 중복된 문자 개수

--- 중복된-문자-개수.js
+++ 중복된-문자-개수.js
@@ -1,10 +1,15 @@
-function solution(array, n) {
-    let result = 0;
-    for (let i in array) {
-        if (array[i] === n) result += 1
-    }
-    return result
+function solution2(array, n) {
+    return array.filter(item => item === n).length;
 }
+
+// 문제를 풀었으나, 적절한 메서드를 사용하지 못함. 위의 filter를 사용했다면 훨씬 간결하고 효율적인 코드를 작성할 수 있었다.
+// function solution(array, n) {
+//     let result = 0;
+//     for (let i in array) {
+//         if (array[i] === n) result += 1
+//     }
+//     return result
+// }
 
 // 코드 실행에서는 통과했으나, 채점에서 실패 결과가 떴던 코드
 // function solution(array, n) {
