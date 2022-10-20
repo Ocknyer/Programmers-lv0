18e4878c731c62f5b5fc26a23b8379c76d315434
Ocknyer <kdu4045@naver.com>
2022-10-21 01:12:31 +0900

    fix : 중복된 문자 개수

--- 중복된-문자-개수.js
+++ 중복된-문자-개수.js
@@ -1,7 +1,16 @@
 function solution(array, n) {
-    let result = [];
-    for (el of array) {
-        if (array.indexOf(el) !== array.lastIndexOf(el)) result.push(el); // 오늘 코테 문제풀이에서 배운 indexOf와 lastIndexOf를 활용해 본 코드
+    let result = 0;
+    for (let i in array) {
+        if (array[i] === n) result += 1
     }
-    return result.length
+    return result
 }
+
+// 코드 실행에서는 통과했으나, 채점에서 실패 결과가 떴던 코드
+// function solution(array, n) {
+//     let result = [];
+//     for (el of array) {
+//         if (array.indexOf(el) !== array.lastIndexOf(el)) result.push(el); // 오늘 코테 문제풀이에서 배운 indexOf와 lastIndexOf를 활용해 본 코드
+//     }
+//     return result.length
+// }
